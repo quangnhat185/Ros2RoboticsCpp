fd7827267a055f1d85955bc4a613f0017af4785f
quangnhat185 <quangnhatnguyenle@gmail.com>
2023-04-10 23:54:24 +0200

    saving fig

--- pub_plot/install/pub_cpp/share/pub_cpp/launch/sin_signal_plot.launch.py
+++ pub_plot/install/pub_cpp/share/pub_cpp/launch/sin_signal_plot.launch.py
@@ -1,22 +1,35 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     ld = LaunchDescription()
 
+    save_img_arg = DeclareLaunchArgument(
+        'save_img',
+        default_value='false',
+        description='Save plot as images'
+    )
+
     sin_pub_node = Node(
         package='pub_cpp',
         executable='sin_pub',
-        name='sin_pub_name'
+        name='sin_pub_name',
     )
 
     plotting_node = Node(
         package='py_plotting',
         executable='sin_plotting',
-        name='sin_plotting_name'
+        name='sin_plotting',
+        parameters=[
+            # {'save_img': LaunchConfiguration('save_img')}
+            {'save_img' : 'true'}
+        ]
     )
 
+    ld.add_action(save_img_arg)
     ld.add_action(sin_pub_node)
     ld.add_action(plotting_node)
 
-    return ld
+    return  ld
--- pub_plot/install/py_plotting/lib/python3.10/site-packages/py_plotting/sin_plotting.py
+++ pub_plot/install/py_plotting/lib/python3.10/site-packages/py_plotting/sin_plotting.py
@@ -3,25 +3,54 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 
 import matplotlib.pyplot as plt
+import os
+import shutil
+import time
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('sin_singla_sub')
+        super().__init__('sin_signal_sub')
+
+
         self.subscription = self.create_subscription(
             Float32MultiArray,
             'sin_signal',
             self.plotting,
             10
         )
+
+        self.declare_parameter('save_img', 'false')
+
+        self.save_img = self.get_parameter('save_img').get_parameter_value().string_value
+
+        # self.save_img = self.get_parameter('save_img').value
+
         self.x = []
         self.y = []
+        self.counter = 0
+
+        if self.save_img == 'true':
+            if  os.path.isdir("./imgs"):
+                shutil.rmtree("./imgs")
+                os.makedirs("./imgs")
+            else:
+                os.makedirs("./imgs")
 
     def plotting(self, msg):
+        self.get_logger().info(f"{self.save_img}")
+        self.get_logger().info(f"{len(self.x)}")
+        
         plt.ylim(-1,1)
         self.x.append(msg.data[0])
         self.y.append(msg.data[1])
         plt.plot(self.x[-100:], self.y[-100:])
+        
+        if (len(self.x) > 100) and (self.save_img=='true'):
+            # self.get_logger().info(f"{os.getcwd()}")
+            plt.savefig(f"./imgs/{self.counter}.png")
+            self.counter+=1
+
         plt.pause(0.01)
         plt.clf()
 
