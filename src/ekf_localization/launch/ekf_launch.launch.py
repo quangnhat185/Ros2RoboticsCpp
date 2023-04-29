0d67f496109306c0ef5c7048b451107ef87a67cc
quangnhat185 <quangnhatnguyenle@gmail.com>
2023-04-29 09:23:03 +0200

    Launch config

---
+++ src/ekf_localization/launch/ekf_launch.launch.py
@@ -0,0 +1,31 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    rviz_config_dir = os.path.join(get_package_share_directory('ekf_localization'),'rviz', 'ekf_rviz.rviz')
+
+    print(rviz_config_dir)
+    # rviz_config_dir = "/home/quang/ros2_robotics_cpp/src/ekf_localization/rviz/ekf_rviz.rviz"
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_dir],
+    )
+
+    ekf_node = Node(
+        package='ekf_localization',
+        executable='ekf_run',
+        name='ekf_node',
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(rviz_node)
+    ld.add_action(ekf_node)
+
+    return ld
