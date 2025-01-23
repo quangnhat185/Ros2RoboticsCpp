8ce4c976ca8e28d80d07fa6b2c2a745be9c52532
Quang Nguyen <lqnnguyen@tudelft.nl>
2025-01-23 23:02:42 +0100

    Remove redundant constants

--- src/hybrid_astar_planning/hybrid_astar_model/include/hybrid_astar_model/hybrid_astar_model.hpp
+++ src/hybrid_astar_planning/hybrid_astar_model/include/hybrid_astar_model/hybrid_astar_model.hpp
@@ -9,17 +9,6 @@ namespace hybrid_astar_model
 
 {
 constexpr double PI = 3.141592;
-
-constexpr double XY_GRID_RESOLUTION = 2.0;                   // [m]
-constexpr double YAW_GRID_RESOLUTION = 15.0 * M_PI / 180.0;  // [rad]
-constexpr double MOTION_RESOLUTION = 0.1;                    // [m] path interpolate resolution
-constexpr int N_STEER = 20;                                  // number of steer command
-
-constexpr double SB_COST = 100.0;          // switch back penalty cost
-constexpr double BACK_COST = 5.0;          // backward penalty cost
-constexpr double STEER_CHANGE_COST = 5.0;  // steer angle change penalty cost
-constexpr double STEER_COST = 1.0;         // steer angle change penalty cost
-constexpr double H_COST = 5.0;             // Heuristic cost
 constexpr double PUB_RATE = 100;
 
 }  // namespace hybrid_astar_model
